4859f0f64ce74a54ae3ba2d82dbcb2d1cabb6808
leane <leane.prad@gmail.com>
2026-02-12 21:42:43 +0100

    Phase 10 : ORM (affichage détaillé d'une visite)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -33,6 +33,12 @@ class VoyagesController extends AbstractController {
         $visites = $this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
     }
+    
+    #[Route('/voyages/voyage/{id}', name : 'voyage.showone')]
+    public function showOne($id) : Response {
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", ['visite' => $visite]);
+    }
 
     /**
      * 
