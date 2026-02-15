f1cbdb8366ae794701f278a44f1514b125ac2843
leane <leane.prad@gmail.com>
2026-02-15 23:00:51 +0100

    Phase 12 : ORM (formulaire pour modifier une visite)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -2,30 +2,50 @@
 
 namespace App\Controller\admin;
 
+use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * Description of AdminVoyagesController
  *
  * @author leane
  */
-class AdminVoyagesController extends AbstractController{
+class AdminVoyagesController extends AbstractController {
+
     #[Route('/admin', name: 'admin.voyages')]
     public function index(): Response {
         $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
         return $this->render("admin/admin.voyages.html.twig", ['visites' => $visites]);
     }
+
     #[Route('/admin/suppr/{id}', name: 'admin.voyages.suppr')]
-    public function suppr(int $id) : Response{
+    public function suppr(int $id): Response {
         $visite = $this->repository->find($id);
         $this->repository->remove($visite);
         return $this->redirectToRoute('admin.voyages');
     }
-    
-     /**
+
+    #[Route('/admin/edit/{id}', name: 'admin.voyage.edit')]
+    public function edit(int $id, Request $request): Response {
+        $visite = $this->repository->find($id);
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+
+        $formVisite->handleRequest($request);
+        if ($formVisite->isSubmitted() && $formVisite->isValid()) {
+            $this->repository->add($visite);
+            return $this->redirectToRoute('admin.voyages');
+        }
+
+        return $this->render("admin/admin.voyage.edit.html.twig", [
+                    'visite' => $visite,
+                    'formvisite' => $formVisite->createView()]);
+    }
+
+    /**
      * 
      * @var VisiteRepository
      */
