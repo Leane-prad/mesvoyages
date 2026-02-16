c3529413e016f1dd62a6db7fb7281239d637897c
leane <leane.prad@gmail.com>
2026-02-16 21:48:40 +0100

    Phase 13 : ORM (ajout d'une visite)

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -4,37 +4,39 @@ namespace App\Form;
 
 use App\Entity\Visite;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use DateTime;
 
-class VisiteType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class VisiteType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            ->add('ville')
-            ->add('pays')
-            ->add('datecreation',null, [
-                'widget' => 'single_text',
-                'label' => 'date'
+                ->add('ville')
+                ->add('pays')
+                ->add('datecreation', DateType::class, [
+                    'widget' => 'single_text',
+                    'data' => isset($options['data']) &&
+                    $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
+                    'label' => 'Date'
                 ])
-            ->add('note')
-            ->add('avis')
-            ->add('tempmin',null, [
-                'label' => 't° min'
-            ])
-            ->add('tempmax',null, [
-                'label' => 't° max'
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer'
-            ])
+                ->add('note')
+                ->add('avis')
+                ->add('tempmin', null, [
+                    'label' => 't° min'
+                ])
+                ->add('tempmax', null, [
+                    'label' => 't° max'
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Enregistrer'
+                ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'data_class' => Visite::class,
         ]);
